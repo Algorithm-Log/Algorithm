e389958f17cbc0e5aeb01195456b9b7864ea0b54
l-wanderer01 <jhjjang0423@naver.com>
2026-08-14 21:37:15 +0900

    Solve: [3week] Programmers_타겟넘버 - 장재훈

---
+++ 3week/Programmers_타겟넘버/장재훈/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] numbers;
+    int target;
+    int answer;
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        
+        findTargetNumber(0, 0);
+        
+        return answer;
+    }
+    
+    // DFS
+    public void findTargetNumber(int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) answer++;
+            return;
+        }
+        
+        // 해당 depth에서 값을 더하는 경우
+        findTargetNumber(depth+1, sum+numbers[depth]);
+        // 해당 depth에서 값을 빼는 경우
+        findTargetNumber(depth+1, sum-numbers[depth]);
+    }
+}
