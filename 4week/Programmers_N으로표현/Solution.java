69ac8c6e31100ec24bb7636980197eae0e8afed1
oxoxoxoxox <dudwlsaos132@naver.com>
2026-08-25 12:18:04 +0900

    Solve: [4week] Programmers_N으로표현 - 김영진

---
+++ 4week/Programmers_N으로표현/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int solution(int N, int number) {
+        Set<Integer>[] dp = new HashSet[9];
+        
+        for(int i = 1; i <= 8; i++){
+            dp[i] = new HashSet<>();
+        }
+        
+        
+        String str = "";
+        for(int i = 1; i <= 8; i++){
+            str += N;
+            dp[i].add(Integer.parseInt(str));
+            for(int j = 1; j < i; j++){
+                for(int a : dp[j]){
+                    for(int b : dp[i-j]){
+                        dp[i].add(a+b);
+                        dp[i].add(a-b);
+                        dp[i].add(a*b);
+                        if(b != 0) dp[i].add(a/b);
+                    }
+                }
+            }
+            if(dp[i].contains(number)){
+                return i;
+            }
+    }
+        return -1;
+}
+}
